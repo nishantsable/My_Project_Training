fea0d1d784e6328bd200a8271a8537f119abf2cd
RavekarN <RavekarN@L10ASO-PC013NCQ.rotaryintl.org>
2017-12-27 14:23:18 +0530

    TestCase Updated

---
+++ src/pah.java
@@ -0,0 +1,20 @@
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class pah {
+
+		
+	
+	
+	public static void main(String[] args)
+    {
+        int ar[] = {1, 2, 3, 4, 5};
+        for (int i=0; i<ar.length; i++)
+          System.out.println(ar[i]); //jjj
+    }
+}
